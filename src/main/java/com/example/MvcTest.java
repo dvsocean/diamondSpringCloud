a99b1563faefb307473a609d07514ff3fc43730c
Ocean <dvsocean@icloud.com>
2018-06-16 17:37:18 -0700

    found the req deps to install successfully

--- src/main/java/com/example/MvcTest.java
+++ src/main/java/com/example/MvcTest.java
@@ -1,10 +1,13 @@
 package com.example;
 
+import com.jayway.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.junit.Before;
+
 public class MvcTest {
 
-//    @Before
-//    public void setup() {
-//        RestAssuredMockMvc.standaloneSetup(new FraudDetectionController());
-//    }
+    @Before
+    public void setup() {
+        RestAssuredMockMvc.standaloneSetup(new FraudDetectionController());
+    }
 
 }
